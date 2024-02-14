e12e2d721fddeecbd3be766200328ce717c7038e
zak <zak@zak.zak>
2024-02-14 22:54:46 +0200

    Add more comments

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 ### WORKING WITH MAVEN VERSION 3.8.6 and we will have issues if we used another version or run the pipeline from a Jenkins container
+### Maven version is in the file pom.xml
 
 echo 'The following Maven command installs your Maven-built Java application'
 echo 'into the local Maven repository, which will ultimately be stored in'
