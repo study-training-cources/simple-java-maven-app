1f2f58bc8b8a7bf2892fdf992a97a8ddb4fece7f
zak <zak@zak.zak>
2024-02-14 16:51:54 +0200

    Adding Maven version comment

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+### WORKING WITH MAVEN VERSION 3.8.6
+
 echo 'The following Maven command installs your Maven-built Java application'
 echo 'into the local Maven repository, which will ultimately be stored in'
 echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
